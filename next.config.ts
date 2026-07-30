a2613c9a2746506ced43a28a3453b33c45bf7288
huihuishi <shihui_t@163.com>
2026-07-30 09:05:11 +0800

    fix: add basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  assetPrefix: '/personal-workbench',
+  basePath: '/personal-workbench',
 };
 
 export default nextConfig;
