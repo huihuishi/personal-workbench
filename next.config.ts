50c61efdf90166b53cbccabaa00cebf8178f878a
huihuishi <shihui_t@163.com>
2026-07-30 08:56:12 +0800

    fix: restore next.config for vercel ssr

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
